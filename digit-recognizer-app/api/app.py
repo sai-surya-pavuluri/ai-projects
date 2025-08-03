2971b92ce191d3c4ed00b980f1be0be23bfa537a
sai-surya-p <saisurya.private@gmail.com>
2025-08-03 01:58:41 +0100

    Updated the project to run as single application using same port for front and back end

--- digit-recognizer-app/api/app.py
+++ digit-recognizer-app/api/app.py
@@ -1,16 +1,32 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import tensorflow as tf
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 from PIL import Image
-import io
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+import logging
 
-app = Flask(__name__)
-CORS(app)
+logging.basicConfig(
+    level=logging.INFO,  # Or WARNING/ERROR/DEBUG
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+
+app = Flask(
+    __name__,
+    template_folder=os.path.abspath("../templates"),
+    static_folder=os.path.abspath("../static")
+)
+
+@app.route('/')
+def index():
+    logger.info("Rendering home page")
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    print("Received a request to predict the digit")
+    logger.info("Received a request to predict the digit")
 
     # Get model type from form data
     model_type = request.form.get('model_type', 'simple').lower()
@@ -19,6 +35,7 @@ def predict():
     try:
         model = tf.keras.models.load_model(model_path)
     except Exception as e:
+        logger.error(f"Could not load model: {e}")
         return jsonify({'error': f'Could not load model: {e}'}), 400
 
     file = request.files['file']
@@ -29,7 +46,7 @@ def predict():
     prediction = model.predict(img_array)[0]
     predicted_class = int(np.argmax(prediction))
     confidence = float(np.max(prediction))
-
+    logger.info(f"Prediction: {predicted_class}, Confidence: {confidence}")
     return jsonify({
         'digit': predicted_class,
         'confidence': round(confidence * 100, 2)
